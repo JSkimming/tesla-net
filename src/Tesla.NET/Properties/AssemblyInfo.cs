9aad4faa0f970c481fc48b4f48103b167bf4a0e7
James Skimming <JSkimming@users.noreply.github.com>
2017-12-17 22:44:10 +0000

    Added the Tesla.NET library

---
+++ src/Tesla.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2018 James Skimming. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+#if NET47
+[assembly: AssemblyTitle("Tesla.NET .NET Framework")]
+#else
+[assembly: AssemblyTitle("Tesla.NET .NET Standard")]
+#endif
+
+[assembly: AssemblyDescription("A .NET client library for the Tesla Owner API.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tesla.NET")]
+[assembly: AssemblyCopyright("Copyright © 2018 James Skimming")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
---
+++ src/Tesla.NET/TeslaClient.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2018 James Skimming. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Tesla.NET
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Client for the Tesla Owner API.
+    /// </summary>
+    public class TeslaClient
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TeslaClient"/> class.
+        /// </summary>
+        /// <param name="baseUri">The <see cref="BaseUri"/> of the Tesla Owner API.</param>
+        public TeslaClient(Uri baseUri)
+        {
+            BaseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+        }
+
+        /// <summary>
+        /// Gets the base <see cref="Uri"/> of the Tesla Owner API.
+        /// </summary>
+        public Uri BaseUri { get; }
+    }
+}
