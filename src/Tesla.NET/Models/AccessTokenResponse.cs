ad011476cb33cca81fa268199429bd2b85364714
James Skimming <JSkimming@users.noreply.github.com>
2018-01-24 22:10:49 +0000

    Added interfaces for all the models

--- src/Tesla.NET/Models/AccessTokenResponse.cs
+++ src/Tesla.NET/Models/AccessTokenResponse.cs
@@ -5,13 +5,12 @@ namespace Tesla.NET.Models
 {
     using System;
     using System.Diagnostics;
-    using Newtonsoft.Json;
 
     /// <summary>
     /// The response to an access token or refresh token request.
     /// </summary>
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
-    public class AccessTokenResponse
+    public class AccessTokenResponse : IAccessTokenResponse
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="AccessTokenResponse"/> class.
@@ -40,49 +39,41 @@ namespace Tesla.NET.Models
         /// <summary>
         /// Gets the access token.
         /// </summary>
-        [JsonProperty("access_token")]
         public string AccessToken { get; }
 
         /// <summary>
         /// Gets the type of the <see cref="AccessToken"/>.
         /// </summary>
-        [JsonProperty("token_type")]
         public string TokenType { get; }
 
         /// <summary>
         /// Gets the expiry duration in seconds of the <see cref="AccessToken"/>.
         /// </summary>
-        [JsonProperty("expires_in")]
         public long ExpiresIn { get; }
 
         /// <summary>
         /// Gets the expiry duration of the <see cref="AccessToken"/>.
         /// </summary>
-        [JsonIgnore]
         public TimeSpan ExpiresInTimespan => TimeSpan.FromSeconds(ExpiresIn);
 
         /// <summary>
         /// Gets the UTC <see cref="DateTime"/> when the <see cref="AccessToken"/> expires.
         /// </summary>
-        [JsonIgnore]
         public DateTime ExpiresUtc => EpochConversion.FromSeconds(CreatedAt + ExpiresIn);
 
         /// <summary>
         /// Gets the Epoch timestamp when the <see cref="AccessToken"/> was issued.
         /// </summary>
-        [JsonProperty("created_at")]
         public long CreatedAt { get; }
 
         /// <summary>
         /// Gets the UTC <see cref="DateTime"/> when the <see cref="AccessToken"/> was issued.
         /// </summary>
-        [JsonIgnore]
         public DateTime CreatedUtc => EpochConversion.FromSeconds(CreatedAt);
 
         /// <summary>
         /// Gets the refresh token that can be used to acquire a new <see cref="AccessToken"/>.
         /// </summary>
-        [JsonProperty("refresh_token")]
         public string RefreshToken { get; }
 
         private string DebuggerDisplay => $"{GetType().Name}: {AccessToken.Substring(0, 6)}… Expires {ExpiresUtc:R}";
---
+++ src/Tesla.NET/Models/IResponseDataWrapper.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2018 James Skimming. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Tesla.NET.Models
+{
+    using System;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The wrapper object for a response from the Tesla Owner API.
+    /// </summary>
+    /// <typeparam name="TResponse">The <see cref="Type"/> of the <see cref="Response"/>.</typeparam>
+    public interface IResponseDataWrapper<out TResponse>
+    {
+        /// <summary>
+        /// Gets the <typeparamref name="TResponse"/> object.
+        /// </summary>
+        [JsonProperty("response")]
+        TResponse Response { get; }
+    }
+}
