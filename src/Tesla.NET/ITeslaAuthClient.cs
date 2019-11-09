a4f5de81319882e762e27b8f843ff99fb98556ef
James Skimming <647706+JSkimming@users.noreply.github.com>
2019-11-09 20:40:28 +0000

    Put back default namespaces, and lost formatting

--- src/Tesla.NET/ITeslaAuthClient.cs
+++ src/Tesla.NET/ITeslaAuthClient.cs
@@ -3,6 +3,9 @@
 
 namespace Tesla.NET
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Tesla.NET.Models;
--- src/Tesla.NET/Requests/HttpClientExtensions.cs
+++ src/Tesla.NET/Requests/HttpClientExtensions.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) 2018 James Skimming. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
 namespace Tesla.NET.Requests
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Net;
     using System.Net.Http;
     using System.Net.Http.Headers;
